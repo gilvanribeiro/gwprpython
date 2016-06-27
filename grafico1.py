36a6ebeca9cb2fd8ea2a28d265bd2f1fde8b552f
gwpr <gilvanwpr@hotmail.com>
2016-06-27 03:35:04 -0300

    primeiro Comit

---
+++ grafico1.py
@@ -0,0 +1,53 @@
+import urllib
+import numpy as np
+import matplotlib.pyplot as plt
+
+# http://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/"
+
+# Magnitude > 4.5
+url = urllib.urlopen(source + "4.5_month.csv")
+
+data = url.read().split('\n')[1:-1] #excluir cabecalho e ultima linha, pois esta' vazia
+
+#Algum codigo processando os dados lidos
+
+dadosSeparados=[]
+url.close()
+time = []
+latitude = []
+longitude = []
+mag = []
+place = []
+tamanho = []
+cor = []
+for row in data:
+    dadosSeparados.append(row.split(','))
+
+for row in dadosSeparados:
+#    time.append(row[0])
+    if row[4] > "5":
+        mag.append(float(row[4]))
+        tamanho.append(float(row[4])**4)
+        cor.append(float(row[4])/10.0)
+        latitude.append(float(row[1]))
+        longitude.append(float(row[2]))
+        if "km" not in row[13]:
+            place.append(row[13].replace('"', ''))
+        else:
+            place.append(row[14].replace('"', ''))
+
+for row in cor:
+    print cor
+
+
+plt.figure(figsize=(14,10))
+plt.axes([0, 25,50,75])
+plt.subplot(1,1,1)
+plt.scatter(longitude, latitude, marker='o', color=cor, s=tamanho)
+plt.grid(True, linestyle="-", color='0.75',linewidth=0.75)
+# Restante do codigo para gerar o grafico
+plt.title('Terremotos com magnitude > 4.5 nos ultimos 30 dias')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.show()
---
+++ grafico3.py
@@ -0,0 +1,50 @@
+import urllib
+import numpy as np
+import matplotlib.pyplot as plt
+
+# http://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+
+source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/"
+
+# Magnitude > 4.5
+url = urllib.urlopen(source + "4.5_month.csv")
+
+data = url.read().split('\n')[1:-1] #excluir cabecalho e ultima linha, pois esta' vazia
+
+# Algum codigo processando os dados lidos
+dadosSeparados=[]
+url.close()
+time = []
+latitude = []
+longitude = []
+mag = []
+place = []
+treta = []
+
+for row in data:
+    dadosSeparados.append(row.split(','))
+
+for row in dadosSeparados:
+#    latitude.append(row[1])
+#    longitude.append(row[2])
+#    time.append(row[0])
+    if row[4] > "5":
+        mag.append(int(float(row[4])))
+        if "km" not in row[13]:
+            place.append(row[13].replace('"', ''))
+        else:
+            place.append(row[14].replace('"', ''))
+
+index = [x for x in range(len(mag))]
+print mag
+
+# Algum codigo para continuar configurando o grafico
+
+# Restante do codigo para gerar o grafico
+plt.axes([0.10, 0.25, 0.8, 0.65])
+plt.xticks(index, place,rotation=90,size='small')
+plt.title('Distribuicao diaria de terremotos nos ultimos 31 dias')
+plt.xlabel('Localizacao')
+plt.ylabel('Numero de Terremotos')
+plt.bar(index, mag,color='g')
+plt.show()
